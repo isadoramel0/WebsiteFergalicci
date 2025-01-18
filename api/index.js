8f5225c91c917c5559787be3a1c059bd34e22cfc
Poyo\Jhuan <jhuancarlossd@proton.me>
2025-01-18 17:51:38 -0300

    Corrigindo problema com CORS na iintegração front-back

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import usuarioRoutes from "./routes/usuario.routes.js";
+import cors from "cors"
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
