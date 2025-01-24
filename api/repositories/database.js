4c2bf222baeb12242c3c5acb88aad36950adb0dc
Poyo\Jhuan <jhuancarlossd@proton.me>
2025-01-23 22:03:06 -0300

    Removendo arquivo .env e adicionando .env ao .gitignore; configurando dotenv no index.js e ajustando a conexão com o banco de dados para usar variável de ambiente

--- api/repositories/database.js
+++ api/repositories/database.js
@@ -1,20 +1,22 @@
-import pg from 'pg'
+import pg from "pg";
 
 // Retorna uma conexão com a database fergalicci
 async function connect() {
+  // Se pool existir, retorne a mesma conexão
+  if (global.connection) {
+    return global.connection.connect();
+  }
 
-    // Se pool existir, retorne a mesma conexão
-    if(global.connection){
-        return global.connection.connect()
-    }
+  // Caso contrário, crie e retorne uma nova conexão
+  const pool = new pg.Pool({
+    connectionString:
+      "postgres://postgres:" +
+      process.env.DB_PASSWORD +
+      "@localhost:5432/fergalicci",
+  });
+  global.connection = pool;
 
-    // Caso contrário, crie e retorne uma nova conexão
-    const pool = new pg.Pool({
-        connectionString: "postgres://postgres:12345@localhost:5432/fergalicci"
-    });
-    global.connection = pool;
-
-    return pool.connect();
+  return pool.connect();
 }
 
-export default { connect }
+export default { connect };
--- api/util/token.js
+++ api/util/token.js
@@ -1,7 +1,4 @@
 import jwt from "jsonwebtoken";
-import env from "dotenv";
-
-env.config();
 
 function generateAccessToken(email) {
   return jwt.sign(email, process.env.TOKEN_SECRET, { expiresIn: "10h" });
